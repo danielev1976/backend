e3d5ba4295e25b1d330ac0ba0dc1c5deccd00a4a
danielev1976 <daniellevin1976@gmail.com>
2026-04-01 22:40:08 +0200

    First commit

---
+++ src/test/java/io/danlev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.danlev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
